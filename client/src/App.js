52d06c66598fca88ad44b47c2349198b583adbc3
KimchiChamchi <esm2103@gmail.com>
2022-03-06 08:49:09 +0900

    App.js 구성 (계정 주소 받아옴)

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import getWeb3 from './getWeb3';
+
+import Main from './routes/Main';
+import MyAnimal from './routes/My-animal';
+import SaleAnimal from './routes/Sale-animal';
+
+const App = () => {
+  const [accounts, setAccounts] = useState();
+
+  const init = async () => {
+    try {
+      console.log('유즈이펙트1');
+      // 네트워크 공급자 및 web3 인스턴스를 가져옵니다.
+      const web3 = await getWeb3();
+      // web3를 사용하여 사용자 계정을 가져옵니다.
+      const accounts = await web3.eth.getAccounts();
+      console.log(accounts);
+      setAccounts(accounts);
+      console.log('유즈이펙트');
+    } catch (error) {
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`
+      );
+      console.error(error);
+    }
+  };
+
+  useEffect(async () => {
+    init();
+  }, [accounts]);
+
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" exact element={<Main accounts={accounts} />} />
+          <Route
+            path="my-animal"
+            exact
+            element={<MyAnimal accounts={accounts} />}
+          />
+          <Route
+            path="sale-animal"
+            exact
+            element={<SaleAnimal accounts={accounts} />}
+          />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
